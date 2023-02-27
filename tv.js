7681a911a77c84a211ec20ec342ddd76fd700156
Shaurya Singh <87537412+Nightmonster4@users.noreply.github.com>
2023-02-27 16:05:50 +0530

    Add files via upload

---
+++ tv.js
@@ -0,0 +1,36 @@
+//img = "";
+
+//function draw(){
+    //image(img, 0, 0, 640, 420);
+    //fill("red");
+    //text("Shoes", 75, 75);
+    //noFill();
+    //stroke("red");
+    //rect(60, 60, 530, 350);
+//}
+
+status="";
+
+function preload(){
+    img = loadImage("Tv.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    objectDetecter = ml5.objectDetector("cocossd", modelloaded);
+    document.getElementById("status").innerHTML = "Status : Detecting objects";
+}
+
+function draw(){
+   image(img, 0, 0, 380, 380);
+}
+
+function modelloaded(){
+    console.log("Model loaded!");
+    objectDetecter.detect(canvas, gotresults);
+}
+
+function gotresults(){
+    
+}
